181150f16c963dea7469cf269707986e91140b2a
Umur Gedik <umurgdk@gmail.com>
2022-12-22 13:34:10 +0300

    Initial Commit

---
+++ SpinWheel/SpinWheelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpinWheelApp.swift
+//  SpinWheel
+//
+//  Created by Umur Gedik on 22.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpinWheelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
